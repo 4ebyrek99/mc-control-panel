de8d03608b0cfe72a0ebd75d35a1a23f0001f71f
4ebyrek <vasya-korn71@mail.ru>
2022-11-04 18:42:02 +0600

    Подготовка файлов

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,26 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+    <v-app>
+        <v-main>
+            <router-view/>
+        </v-main>
+    </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+export default {
+name: 'App',
+    data () {
+        return {
+            
+        }
     }
-  }
-}
+};
+</script>
+
+<style lang="scss" scoped>
+    * {
+        padding: 0px;
+        margin: 0px;
+    }
 </style>
---
+++ src/views/homePage.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-container>
+        
+    </v-container>
+</template>
+
+<script>
+
+export default {
+    name: 'homePage',
+        components: {
+
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
